742f5bb3f42a69788d230ce80b423ab93ed1a430
jinrunheng <1175088275@qq.com>
2021-12-16 00:32:48 +0800

    修复 BulkheadCustomerService 的 bug，添加了 @EnableFeignClients 注解

--- bulkhead-customer-service/src/main/java/com/github/bulkheadcustomerservice/BulkheadCustomerServiceApplication.java
+++ bulkhead-customer-service/src/main/java/com/github/bulkheadcustomerservice/BulkheadCustomerServiceApplication.java
@@ -7,6 +7,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
@@ -15,6 +16,7 @@ import java.util.concurrent.TimeUnit;
 @SpringBootApplication
 @Slf4j
 @EnableDiscoveryClient
+@EnableFeignClients
 @EnableAspectJAutoProxy
 public class BulkheadCustomerServiceApplication {
 
